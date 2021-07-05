3db04f0ff5b13a73b4c96be7b0e9702a03717178
Armel Geek <armelgeek5@gmail.com>
2021-07-05 18:26:47 +0200

    taobao frontend get started

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
+import App from "next/app";
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
+import cookies from "next-cookies";
+import UserProvider from "../context/UserContext";
+import Layout from "../components/layout";
+import {wrapper} from '../store';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, isAuthenticated }: any) {
+  return(
+  <UserProvider isAuthenticated={isAuthenticated}>
+    <Layout>
+        <Component {...pageProps} />
+    </Layout>
+  </UserProvider>
+  )
 }
-export default MyApp
+
+MyApp.getInitialProps = async (context) => {
+  let isAuthenticated = false;
+
+  // WARNING - We only check if a cookie called token is present
+  // We do not verify the token on the server at this point
+  // In this case, it might be fine since we only need the auth state
+  // for UI purposes. Any sensitive data fetch is secured separately
+  const { token } = cookies(context.ctx);
+  if (token) {
+    isAuthenticated = true;
+  }
+
+  const appProps = await App.getInitialProps(context);
+  return { ...appProps, isAuthenticated };
+};
+
+export default wrapper.withRedux(MyApp);
